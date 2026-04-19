8b1c92886970581e1653809cd1395e24b4a9dd4a
Barbara Noguera <barbarang20@gmail.com>
2026-04-18 20:43:24 -0400

    fix: CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import libros, usuarios, prestamos
 
 app = FastAPI(
     title="Bolsa del Libro - Ingeniería Eléctrica UCV",
     description="Sistema de gestión bibliotecaria con arquitectura de microservicios",
     version="1.0.0"
+)
+
+# Permitir peticiones desde el frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(libros.router)
